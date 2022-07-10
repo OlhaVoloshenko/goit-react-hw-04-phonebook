3c3a545cf3e21fec81e8d1b2244657f3c3344452
OlhaVoloshenko <voloshenko.olya@gmail.com>
2022-07-10 18:18:37 +0300

    update files json
    update files json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import { ThemeProvider } from '@emotion/react';
 import { App } from 'components/App';
 import './index.css';
+//import { theme } from 'constans';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
